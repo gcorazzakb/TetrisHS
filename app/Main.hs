08fa9fa7f7ace70ebabdfa1cc1cbfa1476ce52e7
Gianluca Corazza <gianluca.corazza@klosebrothers.com>
2020-07-28 17:42:47 +0200

    step, reformat, tetrisz

--- app/Main.hs
+++ app/Main.hs
@@ -1,62 +1,89 @@
+{-# LANGUAGE BlockArguments #-}
+
 module Main where
 
-import Lib
-
 import Graphics.Gloss
-
+import Lib
 
 main :: IO ()
 main = display (InWindow "Nice Window" (200, 200) (10, 10)) white $ drawField initField
 
 type Pos = (Int, Int)
+
 type Map = [(Pos, Color)]
+
 type Tetris = Map
+
 type Shape = [Pos]
 
-data GameState = GameState {
-  field :: Map,
-  tetris :: Tetris,
-  x :: Int,
-  y :: Int
-}
+data GameState = GameState
+  { field :: Map,
+    tetris :: Tetris,
+    pos :: Pos
+  }
 
 drawField :: Map -> Picture
-drawField field = Pictures $ (\((x,y),c) -> color c (drawRect (Rectangle (x* 10,y * 10) (10,10)))) <$> field
+drawField field = Pictures $ (\((x, y), c) -> color c (drawRect (Rectangle (x * 10, y * 10) (10, 10)))) <$> field
 
 initField :: Map
-initField = [((1,1),blue),((1,2),blue), ((1,3),red), ((2,1),red)]
+initField = [((1, 1), blue), ((1, 2), blue), ((1, 3), red), ((2, 1), red)]
 
 drawRect :: Rectangle -> Picture
 drawRect rect = translate (fromIntegral px) (fromIntegral py) $ uncurry rectangleSolid (fromIntegral x, fromIntegral y)
-  where (x,y) = rectSize rect
-        (px, py) = rectPos rect
+  where
+    (x, y) = rectSize rect
+    (px, py) = rectPos rect
 
 colorizeShape :: Shape -> Color -> Tetris
-colorizeShape s c = (\(x,y) -> ((x,y),c)) <$> s
+colorizeShape s c = (\(x, y) -> ((x, y), c)) <$> s
+
+tetriz :: [Tetris]
+tetriz = [
+  colorizeShape l $ bright red,
+  colorizeShape (mirrored l) $ bright blue,
+  colorizeShape z $ bright green,
+  colorizeShape (mirrored z) $ bright magenta,
+  colorizeShape i $ bright cyan,
+  colorizeShape i $ bright orange]
 
 l :: Shape
-l = [(0,0), (0,1), (0,2), (1,0)]
+l = [(0, 0), (0, 1), (0, 2), (1, 0)]
 
 z :: Shape
-z = [(0,0), (0,1), (1,1), (1,2)]
+z = [(0, 0), (0, 1), (1, 1), (1, 2)]
 
 i :: Shape
-i = [(0,0), (0,1), (0,2), (0,3)]
+i = [(0, 0), (0, 1), (0, 2), (0, 3)]
 
 o :: Shape
-o = [(0,0), (0,1), (1,0), (1,1)]
+o = [(0, 0), (0, 1), (1, 0), (1, 1)]
 
 mirrored :: Shape -> Shape
-mirrored s = (\(x,y) -> (-x,y)) <$> s
+mirrored s = (\(x, y) -> (- x, y)) <$> s
 
 rotate :: Shape -> Shape
-rotate s = (\(x,y) -> (-y,x)) <$> s
+rotate s = (\(x, y) -> (- y, x)) <$> s
 
 getShape :: Map -> Shape
 getShape m = fst <$> m
 
-colidesWithMap :: Map -> Tetris -> Bool
-colidesWithMap m t = colides (getShape m) (getShape t)
-
 colides :: Shape -> Shape -> Bool
-colides m t = any (\(x,y) -> (x, y) `elem` t) m
+colides m t = any (\(x, y) -> (x, y) `elem` t) m
+
+translateShape :: Shape -> Pos -> Shape
+translateShape s (x1, y1) = (\(x, y) -> (x + x1, y + y1)) <$> s
+
+anker :: Map -> Tetris -> Map
+anker m t = m ++ t
+
+getRandomTetris :: Tetris
+getRandomTetris = head tetriz
+
+step :: GameState -> GameState
+step (GameState m t (x, y)) =
+  GameState
+    (if colidesWithMapWhenDrop then anker m t else m)
+    (if colidesWithMapWhenDrop then getRandomTetris else t)
+    (if colidesWithMapWhenDrop then (3, 0) else (x, y + 1))
+  where
+    colidesWithMapWhenDrop = colides (translateShape (getShape t) (x, y + 1)) (getShape m)
